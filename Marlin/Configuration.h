a501ee372577b48e7b009601a6d14ca46cf47ff5
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-11-22 11:25:58 -0600

    adjust for bed centering

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1393,7 +1393,7 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS -2.5
+#define MANUAL_X_HOME_POS 12.5
 #define MANUAL_Y_HOME_POS -15.5
 //#define MANUAL_Z_HOME_POS 0
 
