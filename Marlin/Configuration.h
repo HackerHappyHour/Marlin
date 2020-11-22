e9d4afc8f372f3830a821e23f978d5993a0a597b
Brian Vanderbusch <chief@hackerhappyhour.com>
2020-11-21 18:03:35 -0600

    adjust probe offset
    making adjustments for using a new compact mount for
    hemera where probe is to the right of nozzle

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1007,7 +1007,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43, 0, -0.350 }
+#define NOZZLE_TO_PROBE_OFFSET { 62.5, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
