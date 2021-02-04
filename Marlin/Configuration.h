3b32a9e4689ce1b881c6f5543170863cacdf0ed8
Brian Vanderbusch <chief@hackerhappyhour.com>
2021-02-04 09:21:43 -0600

    set z positioning configs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1128,10 +1128,10 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -60
+#define Z_MIN_POS -40
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 0
+#define Z_MAX_POS 40
 
 /**
  * Software Endstops
@@ -1374,7 +1374,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS -60
+//#define MANUAL_Z_HOME_POS -60
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
