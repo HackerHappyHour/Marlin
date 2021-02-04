366c34eb235c5b4b98179cec23a05799459751ca
Brian Vanderbusch <chief@hackerhappyhour.com>
2021-02-04 08:32:57 -0600

    tweak bed geometry

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1122,16 +1122,16 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 160
+#define Y_BED_SIZE 100
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
-#define Z_MIN_POS -100
+#define Z_MIN_POS -60
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 0
 
 /**
  * Software Endstops
@@ -1374,7 +1374,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS -98
+#define MANUAL_Z_HOME_POS -60
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
 //
